6fbb7d2bc09bcf06aba64a2be6740934a1294bd9
xaos <vladimir.p.polyakov@gmail.com>
2017-08-23 17:29:20 +0500

    clean

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,12 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableConfigurationProperties
 public class DemoApplication {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -4,7 +4,6 @@ import com.example.config.SampleProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,8 +13,7 @@ public class IndexController {
     SampleProperty sampleProperty;
 
     @RequestMapping("/")
-    public ModelAndView index(
-            @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+    public ModelAndView index() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("index");
         mv.getModel().put("prop1", sampleProperty.getStringProp1());
